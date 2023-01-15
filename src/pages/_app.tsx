b7e1e4911a8788c3a45fe9a1a9642a70e37c62c6
feripe1 <89111098+feripe1@users.noreply.github.com>
2023-01-15 16:02:40 -0300

    meta data update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,8 @@ import { QueryClientProvider, QueryClient, Hydrate, DehydratedState } from 'reac
 import { ReactNode } from 'react';
 import Image from 'next/image';
 import Spinner from '../assets/Spinner.svg';
+import Head from 'next/head';
+import ogimage from '../assets/ogimage.png';
 
 const queryClient = new QueryClient();
 
@@ -35,21 +37,52 @@ export default function myApp({
 const Layout = ({ children }: Props) => {
   const session = useSession();
 
-  if (session.status === 'loading') {
-    return (
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '100vh',
-          flexDirection: 'column',
-        }}
-      >
-        <h2>Show Maker</h2>
-        <Image src={Spinner} alt="" priority loading="eager" height={64} width={64} />
-      </div>
-    );
-  }
-  return <>{children}</>;
+  return (
+    <>
+      <Head>
+        <title>Show Maker</title>
+        <meta
+          name="description"
+          content="O maior rede social já feita. Elon Musk me contrate"
+        />
+        <meta property="og:url" content={`https://show-maker.vercel.app`} />
+        <meta property="og:title" content="Show Maker" />
+        <meta
+          property="og:description"
+          content="O maior rede social já feita. Elon Musk me contrate"
+        />
+        <meta property="og:site_name" content="Show Maker" />
+        <meta property="og:image" content={ogimage.src} />
+        <meta property="og:type" content="website" />
+
+        <meta name="twitter:site" content="@showmaker" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="show-maker.vercel.app" />
+        <meta property="twitter:url" content={`https://show-maker.vercel.app`} />
+        <meta name="twitter:title" content="Show Maker" />
+        <meta
+          name="twitter:description"
+          content="O maior rede social já feita. Elon Musk me contrate"
+        />
+        <meta name="theme-color" content="#fff" />
+      </Head>
+
+      {session.status === 'loading' ? (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '100vh',
+            flexDirection: 'column',
+          }}
+        >
+          <h2>Show Maker</h2>
+          <Image src={Spinner} alt="" priority loading="eager" height={64} width={64} />
+        </div>
+      ) : (
+        <>{children}</>
+      )}
+    </>
+  );
 };
