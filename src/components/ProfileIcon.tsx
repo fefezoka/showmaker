9ed3cda4db524008371c55c384b55e5963f83c13
feripe1 <89111098+feripe1@users.noreply.github.com>
2023-03-21 14:22:33 -0300

    add profile icon fallback image

--- src/components/ProfileIcon.tsx
+++ src/components/ProfileIcon.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useState } from 'react';
 import { CSS, styled } from '../../stitches.config';
+import discordFallback from '../assets/discord-fallback.png';
 
 interface Props extends React.ComponentProps<typeof Image> {
   src: string;
@@ -15,9 +16,18 @@ export const Wrapper = styled('div', {
 
 export const ProfileIcon = forwardRef<HTMLImageElement, Props>(
   ({ src, css, ...props }: Props, forwardedRef) => {
+    const [error, setError] = useState(false);
+
     return (
       <Wrapper css={{ size: '$7', br: '$round', ...css }}>
-        <Image ref={forwardedRef} src={src} {...props} fill alt="" />
+        <Image
+          ref={forwardedRef}
+          src={error ? discordFallback : src}
+          {...props}
+          fill
+          alt=""
+          onError={() => setError(true)}
+        />
       </Wrapper>
     );
   }
