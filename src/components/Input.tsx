4b79dffb907b55a3511eefe13735ce080362fa8d
feripe1 <89111098+fefezoka@users.noreply.github.com>
2023-04-18 02:37:01 -0300

    fix input ref error

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -47,22 +47,21 @@ const StyledInput = styled('input', {
   },
 });
 
-export const Input = React.forwardRef<
-  React.ElementRef<typeof StyledInput>,
-  React.ComponentProps<typeof StyledInput>
->(({ Icon, ...props }: InputProps, forwardedRef) => {
-  return (
-    <Box css={{ size: '100%', position: 'relative' }}>
-      <StyledInput {...props} ref={forwardedRef} />
-      {Icon && (
-        <Box css={{ position: 'absolute', right: '$4', top: '$3' }}>
-          <Box as={'button'} type="submit">
-            <Icon color="white" />
+export const Input = React.forwardRef<React.ElementRef<typeof StyledInput>, InputProps>(
+  ({ Icon, ...props }: InputProps, forwardedRef) => {
+    return (
+      <Box css={{ size: '100%', position: 'relative' }}>
+        <StyledInput {...props} ref={forwardedRef} />
+        {Icon && (
+          <Box css={{ position: 'absolute', right: '$4', top: '$3' }}>
+            <Box as={'button'} type="submit">
+              <Icon color="white" />
+            </Box>
           </Box>
-        </Box>
-      )}
-    </Box>
-  );
-});
+        )}
+      </Box>
+    );
+  }
+);
 
 Input.displayName = 'Input';
